03d4661798e29b8fccd66f52b684a960b6e2fa46
Jeremy Roquin <jeremy.roquin@mastertp>
2020-01-10 17:06:57 +0100

    Added a js file

---
+++ index.js
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<h1 style="color: #5e9ca0;">Bienvenue chez <span style="color: #2b2301;">Cpe-Net</span> !</h1>
+<h2 style="color: #2e6c80;">Et voici une poule</h2>
+<img style="width: 20%" src="https://www.stickers-muraux.fr/ar-stickers-poule-4768.jpg"/>
+</html>
